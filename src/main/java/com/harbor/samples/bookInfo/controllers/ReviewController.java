fc39cd0615eaaea2fb1a9b33eb97322ce8e1e02b
Tajammul Pasha <tajammul@goharbor.xyz>
2024-01-26 13:25:27 +0530

    fix: #1294

--- src/main/java/com/harbor/samples/bookInfo/controllers/ReviewController.java
+++ src/main/java/com/harbor/samples/bookInfo/controllers/ReviewController.java
@@ -29,10 +29,7 @@ public class ReviewController {
     @PostMapping(value = "/review", consumes = "application/json", produces = "application/json")
     public ReviewDTO createReview(@RequestBody ReviewDTO reviewDto) {
         LOGGER.info("Creating review: {}", reviewDto);
-        LOGGER.info("Creating review: {}", reviewDto);
-
-        LOGGER.info("Creating review: {}", reviewDto);
-
+        
         Review review = dtoUtils.convertToReview(reviewDto);
         LOGGER.info("Mapped review: {}", review);
         return dtoUtils.convertToReviewDTO(reviewService.createReview(review));
